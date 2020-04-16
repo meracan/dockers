6104108de451892a857a5fb44a737c949e494ffe
Julien Cousineau <julien.cousineau@gmail.com>
2020-04-16 03:04:02 +0000

    Corrected telemac dockerfile

---
+++ telemac/run.py
@@ -0,0 +1,10 @@
+import os
+import s3telemac
+
+# Get environment variables
+CAS_S3_URL = os.getenv('CAS_S3_URL')
+
+TELAMAC_MODULE = os.getenv('TELAMAC_MODULE')
+CORES = os.getenv('TELAMAC_CORES')
+
+s3telemac(CAS_S3_URL)
